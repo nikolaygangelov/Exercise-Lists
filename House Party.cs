2cc448432f6e61bbd3f502b0ce405e01e0399222
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-19 00:30:32 +0300

    Add files via upload

---
+++ House Party.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace _3._House_Party
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> guestList = new List<string>();
+
+            while (numberOfCommands > 0)
+            {
+                string command = Console.ReadLine();
+                string[] commandParameters = command.Split();
+                string firstCommandParameter = commandParameters[0];
+                string secondCommandParameter = commandParameters[1];
+                string thirdCommandParameter = commandParameters[2];
+
+                if(secondCommandParameter == "is" && thirdCommandParameter == "going!")
+                {
+                    if (!guestList.Contains(firstCommandParameter))
+                    {
+                        guestList.Add(firstCommandParameter);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{firstCommandParameter} is already in the list!");
+                    }
+                }
+                else if(secondCommandParameter == "is" && thirdCommandParameter == "not")
+                {
+                    if (guestList.Contains(firstCommandParameter))
+                    {
+                        guestList.Remove(firstCommandParameter);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{firstCommandParameter} is not in the list!");
+                    }
+                }
+
+                numberOfCommands--;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, guestList));
+        }
+    }
+}
