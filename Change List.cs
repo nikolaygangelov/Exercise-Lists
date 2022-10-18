61d305bf01f2a6cf74ef1667ef3a3ff992703680
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-18 23:42:39 +0300

    Add files via upload

---
+++ Change List.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace _2._Change_List
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> items = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] inputParameters = command
+                    .Split()
+                    .ToArray();
+
+                string firstParameter = inputParameters[0];
+                int secondParameter = int.Parse(inputParameters[1]);
+
+                if(firstParameter == "Delete")
+                {
+                    if (items.Contains(secondParameter))
+                    {
+                        items.RemoveAll(x => x == secondParameter);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if(firstParameter == "Insert")
+                {
+                    int index = int.Parse(inputParameters[2]);
+
+                    if (index < 0 || index >= items.Count)
+                    {
+                        continue;
+                    }
+                    items.Insert(index, secondParameter);
+                }
+            }
+            Console.WriteLine(string.Join(' ', items));
+        }
+    }
+}
